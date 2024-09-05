9c0b7ea64307d893e95aae48e5e30b81551c7314
Hai.Nguyen <haingdc@gmail.com>
2024-09-05 19:28:41 +0700

    Update site metadata and localize text; upgrade Next.js and package manager versions

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Gần đây
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -22,6 +22,7 @@ export default function Home({ posts }) {
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
+            console.log('inspect.posts', date, siteMetadata.locale, formatDate(date, siteMetadata.locale))
             return (
               <li key={slug} className="py-12">
                 <article>
@@ -59,7 +60,7 @@ export default function Home({ posts }) {
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                           aria-label={`Read more: "${title}"`}
                         >
-                          Read more &rarr;
+                          Xem tiếp &rarr;
                         </Link>
                       </div>
                     </div>
@@ -77,7 +78,7 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="All posts"
           >
-            All Posts &rarr;
+           Tất cả bài viết &rarr;
           </Link>
         </div>
       )}
